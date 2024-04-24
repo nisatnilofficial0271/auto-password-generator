ac394aa942cbd22304a30ef502a8146ff581cdda
Nisat Ahmed Nil <107569069+nisatnilofficial0271@users.noreply.github.com>
2024-04-24 08:14:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('generate-btn').addEventListener('click', function() {
+    var length = 12;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    document.getElementById('password').value = password;
+});
